1f6e7c8f4e4fcbf202da82957bb670ed6f694649
Shreya Krishnakumar <02shreya.k@gmail.com>
2026-04-06 16:13:33 +0530

    Update run.py

---
+++ scripts/run.py
@@ -0,0 +1,19 @@
+from environment import CrisisEnv
+from agent import decide_action
+
+env = CrisisEnv()
+
+obs = env.reset()
+
+done = False
+total_reward = 0
+
+while not done:
+    action = decide_action(obs)
+    print("ACTION:", action)
+    
+    obs, reward, done, _ = env.step(action)
+    
+    total_reward += reward
+
+print("FINAL SCORE:", total_reward)
